917f9ed621bb2e79c5e5236ed09d29e34c98a3da
Nawod <nawodrandima64@gmail.com>
2021-01-14 22:25:32 +0530

    added footer

--- homepage.php
+++ homepage.php
@@ -15,6 +15,9 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet"> -->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
 
 </head>
 
@@ -88,9 +91,221 @@
     <section id="jobs">
         <div class="container">
             <h5>RECENT UPDATES</h5>
-            <div
+            <div class="campany-details">
+                <div class="job-update">
+                    <h4><b>seasoned senior python developer</b></h4>
+                    <p>abc company Pvt Ltd</p>
+                    <i class="fa fa-briefcase"></i><span>0-1yrs</span><br>
+                    <i class="fa fa-money"></i><span>rs. 40,000.00</span><br>
+                    <i class="fa fa-map-marker"></i><span>colombo</span><br>
+                    <p>skills <i class="fa fa-angle-double-right"></i><small>java</small><small>php</small></p>
+                    <p>Description <i class="fa fa-angle-double-right"></i> mvkfdmvkkgkkvrkrlvkfklfklfkl lplgplplg<a href="#">Read more</a></p>
+                </div>
+                <div class="apply-btn">
+                    <button type="button" class="btn btn-primary">Apply</button>
+                </div>
+            </div>
+
+            <div class="campany-details">
+                <div class="job-update">
+                    <h4><b>seasoned senior python developer</b></h4>
+                    <p>abc company Pvt Ltd</p>
+                    <i class="fa fa-briefcase"></i><span>0-1yrs</span><br>
+                    <i class="fa fa-money"></i><span>rs. 40,000.00</span><br>
+                    <i class="fa fa-map-marker"></i><span>colombo</span><br>
+                    <p>skills <i class="fa fa-angle-double-right"></i><small>java</small><small>php</small></p>
+                    <p>Description <i class="fa fa-angle-double-right"></i> mvkfdmvkkgkkvrkrlvkfklfklfkl lplgplplg<a href="#">Read more</a></p>
+                </div>
+                <div class="apply-btn">
+                    <button type="button" class="btn btn-primary">Apply</button>
+                </div>
+            </div>
+
+            <div class="campany-details">
+                <div class="job-update">
+                    <h4><b>seasoned senior python developer</b></h4>
+                    <p>abc company Pvt Ltd</p>
+                    <i class="fa fa-briefcase"></i><span>0-1yrs</span><br>
+                    <i class="fa fa-money"></i><span>rs. 40,000.00</span><br>
+                    <i class="fa fa-map-marker"></i><span>colombo</span><br>
+                    <p>skills <i class="fa fa-angle-double-right"></i><small>java</small><small>php</small></p>
+                    <p>Description <i class="fa fa-angle-double-right"></i> mvkfdmvkkgkkvrkrlvkfklfklfkl lplgplplg<a href="#">Read more</a></p>
+                </div>
+                <div class="apply-btn">
+                    <button type="button" class="btn btn-primary">Apply</button>
+                </div>
+            </div>
+
+            <div class="campany-details">
+                <div class="job-update">
+                    <h4><b>seasoned senior python developer</b></h4>
+                    <p>abc company Pvt Ltd</p>
+                    <i class="fa fa-briefcase"></i><span>0-1yrs</span><br>
+                    <i class="fa fa-money"></i><span>rs. 40,000.00</span><br>
+                    <i class="fa fa-map-marker"></i><span>colombo</span><br>
+                    <p>skills <i class="fa fa-angle-double-right"></i><small>java</small><small>php</small></p>
+                    <p>Description <i class="fa fa-angle-double-right"></i> mvkfdmvkkgkkvrkrlvkfklfklfkl lplgplplg<a href="#">Read more</a></p>
+                </div>
+                <div class="apply-btn">
+                    <button type="button" class="btn btn-primary">Apply</button>
+                </div>
+            </div>
+
+            <ul class ="pagelink text-center">
+                <li class="left-arrow">&#8592;</li>
+                <li class="active">1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li class="right-arrow">&#8594;</li>
+            </ul>
         </div>
     </section>
+
+    <!-- site status -->
+    <section id="site-status">
+        <div class="container text-center">
+            <h3>jobak.lk SITE STATUS</h3>
+            <div class="row">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="stats-box">
+                            <i class="fa fa-user-o"></i><span><small>100k +</small></span>
+                            <p>job seekers</p>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="stats-box">
+                            <i class="fa fa-slideshare"></i><span><small>500 +</small></span>
+                            <p>Employers</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="stats-box">
+                            <i class="fa fa-hand-peace-o"></i><span><small>10k +</small></span>
+                            <p>active jobs</p>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="stats-box">
+                            <i class="fa fa-building-o"></i><span><small>400 +</small></span>
+                            <p>campanies</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <!--Start Footer Section -->
+    <footer class="container-fluid page-footer font-small pt-4 bg-dark text-light">
+        <!-- Footer Links -->
+        <div class="container text-center text-md-left">
+            <!-- Footer links -->
+            <div class="row text-center text-md-left mt-3 pb-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3 ">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">Riyasewana Srilanka</h6>
+                    <p>
+                        Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit.
+                    </p>
+                </div>
+                <!-- Grid column -->
+                <hr class="w-100 clearfix d-md-none">
+                <!-- Grid column -->
+                <!-- Grid column -->
+                <hr class="w-100 clearfix d-md-none">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold ">Useful links</h6>
+                    <p>
+                        <a class="text-decoration-none" href="homepage.html">Home</a>
+                    </p>
+                    <p>
+                        <a class="text-decoration-none" href="addpost.html">Post FREE Vehicle AD </a>
+                    </p>
+                    <p>
+                        <a class="text-decoration-none" href="contactus.html">Contact us</a>
+                    </p>
+                    <p>
+                        <a class="text-decoration-none" href="#!">Contribute</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+                <hr class="w-100 clearfix d-md-none">
+                <!-- Grid column -->
+                <div id="contact" class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h6 id="contacus" class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                    <p>
+                        <i class="fas fa-home mr-3"></i> No :- 15/1, Awissawella
+                    </p>
+                    <p>
+                        <i class="fas fa-envelope mr-3"></i> infogmail@gmail.com
+                    </p>
+                    <p>
+                        <i class="fas fa-phone mr-3"></i> + 01 234 567 88
+                    </p>
+                    <p>
+                        <i class="fas fa-print mr-3"></i> + 01 234 567 89
+                    </p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Footer links -->
+            <hr>
+            <!-- Grid row -->
+            <div class="row d-flex align-items-center">
+                <!-- Grid column -->
+                <div class="col-md-4">
+                    <div>
+                        <div class="">
+                            <p>Copyright ..&copy; <span id="year"></span></p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Grid column -->
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-4 ml-lg-0">
+                    <!-- Social buttons -->
+                    <div class="text-center text-md-right">
+                        <ul class="list-unstyled list-inline">
+                            <li class="list-inline-item">
+                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                    <i class="fab fa-google-plus-g"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                    <i class="fab fa-linkedin-in"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+        <!-- Footer Links -->
+    </footer>
+    <!--End Footer Section -->
 </body>
 
 </html>
